f16b0f761179a11ca5fbad7ff1d1dc231493e95c
Nikhil Prabhakar <nprabhak@unmac.local>
2013-01-09 13:55:08 +0530

    Code snippets from http://0branch.com/notes/tco-cl.html.

---
+++ scratch/exploration.lisp
@@ -0,0 +1,23 @@
+(in-package :cl-fun)
+
+;;; http://0branch.com/notes/tco-cl.html
+(defun foo () (foo))
+
+;; (disassemble 'foo)
+
+(defun bar () (foo))
+
+;; (disassemble 'bar)
+
+(defun baz ()
+  (format t "Hello")
+  (bar))
+
+;; (disassemble 'baz)
+
+;; (declaim (optimize #+sbcl (sb-c::merge-tail-calls 3)
+;;                    #+sbcl (sb-c::insert-debug-catch 0)))
+
+;; (declaim (optimize (debug 0)
+;;                    (safety 0)
+;;                    (speed 3)))
